01cbc996fb92a7e6d80e4c3e7d14e4ba2726a8fb
unknown <hao.zhou@cnsha-16789-win.dena.local>
2016-03-21 14:19:03 +0800

    keystore setting

---
+++ Assets/Editor/PlayerSettingHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PlayerSettingHelper : Editor
+{
+    [InitializeOnLoad]
+    public class KeyStoreHelper
+    {
+        static string m_key_store_path = "Assets//Editor//denachina.keystore";
+        static string m_key_alias_name = "dena";
+        static string m_key_store_pass = "denadena01";
+        static string m_key_alias_pass = "denadena01";
+#if UNITY_ANDROID
+        static keyStoreHelper()
+        {
+            string path = Path.GetFullPath(m_key_store_path);
+            PlayerSettings.Android.keystoreName = path;
+            PlayerSettings.Android.keyaliasName = m_key_alias_name;
+            PlayerSettings.Android.keystorePass = m_key_store_pass;
+            PlayerSettings.Android.keyaliasPass = m_key_alias_pass;
+        }
+#endif
+    }
+}
