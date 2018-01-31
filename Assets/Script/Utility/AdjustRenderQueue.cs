e017de9bce2fbf7759ac03eef140a1abf12b0b81
nickchow007 <nickchow.workroom@gmail.com>
2018-01-31 19:26:55 +0800

    20180131

---
+++ Assets/Script/Utility/AdjustRenderQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustRenderQueue : MonoBehaviour
+{
+
+    public int RenderQueue = 3000;
+    private int renderQueue
+    {
+        get
+        {
+            if (RenderQueue < 1) 
+                RenderQueue = 1;
+            return RenderQueue;
+        }
+    }
+
+
+
+    void Update()
+    {
+        Renderer[] renders = transform.GetComponentsInChildren<Renderer>();
+        if (renders != null)
+        {
+            for (int i = 0; i < renders.Length; ++i)
+            {
+                if (renders[i].material != null)
+                    renders[i].sharedMaterial.renderQueue = renderQueue;
+            }
+        }
+
+    }
+
+}
